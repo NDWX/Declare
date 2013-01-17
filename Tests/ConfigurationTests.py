aec2a7f5a0054cbc73025a589919e3cee902127c
Andrian <Andrian@ND.id.au>
2013-01-17 20:38:21 +1100

    Modified test cases for more comprehensive scenarios.
    - Modified Configuration.json
    - Modified ConfigurationTests.py
    - Modified ManagerTests.py

--- Tests/ConfigurationTests.py
+++ Tests/ConfigurationTests.py
@@ -3,42 +3,77 @@ __author__ = 'ND'
 import unittest
 import Declare
 
+
 class ConfigurationTestCase(unittest.TestCase):
+	"""
+	Test configuration reader
+	"""
 
 	_configuration_ = None
 
 	def setUp(self):
+		"""
+		read configuration from 'configuration.json'
+		"""
 		self._configuration_ = Declare.Configuration.read("configuration.json")
 
 	def test_successful_read(self):
+		"""
+		check that configuration was successfully read
+		"""
 		self.assertTrue(self._configuration_ is not None)
 
 	def test_resource_exists(self):
-		self.assertTrue(self._configuration_.resources().has_key("RepeatableTaskRepeat"))
+		"""
+		check that a declared resource exists
+		"""
+		self.assertTrue(self._configuration_.resources().has_key("AddWordTaskRepeat") and self._configuration_.resources().has_key("RemoveWordTaskRepeat"))
 
 	def test_resource_value(self):
-		self.assertTrue(self._configuration_.resources()["RepeatableTaskRepeat"] == True)
+		"""
+		check resource value is as expected
+		"""
+		self.assertTrue(self._configuration_.resources()["RemoveWordTaskRepeat"] == False)
 
 	def test_component_specifications_exist(self):
+		"""
+		check all specified component exists
+		"""
 		self.assertTrue(not (self._configuration_["AddWordDefinitionTask"] is None
 		                     or self._configuration_["ListWordDefinitionsTask"] is None or
 		                     self._configuration_["RemoveWordDefinitionTask"] is None))
 
 	def test_component_class_and_module(self):
+		"""
+		check specified component has correct class and module name
+		"""
 		self.assertTrue(self._configuration_["AddWordDefinitionTask"].class_name() == "AddWordDefinitionTask" and
 		                self._configuration_["AddWordDefinitionTask"].module_name() == "TestModel")
 
 	def test_component_specifications_init_arg_list(self):
-		self.assertTrue("{$RepeatableTaskRepeat}" in self._configuration_["AddWordDefinitionTask"].init_args() )
+		"""
+		check specified component has correct __init__ argument list
+		"""
+		self.assertTrue("{$AddWordTaskRepeat}" in self._configuration_["AddWordDefinitionTask"].init_args() )
 
 	def test_component_specifications_init_arg_dict(self):
+		"""
+		check specified component uses dictionary argument list and that it contains contains specific argument name with specific value
+		"""
 		init_args = self._configuration_["RemoveWordDefinitionTask"].init_args()
-		self.assertTrue(init_args.has_key("repeat") and init_args["repeat"] == "{$RepeatableTaskRepeat}")
+
+		self.assertTrue(type(init_args) == dict and init_args.has_key("repeat") and init_args["repeat"] == "{$RemoveWordTaskRepeat}")
 
 	def test_component_specification_lifetime_declaration(self):
+		"""
+		check that specific component specification has correct lifetime declaration
+		"""
 		self.assertTrue(self._configuration_["ListWordDefinitionsTask"].lifetime() == "singleton")
 
 	def test_component_specification_lifetime_non_declaration(self):
+		"""
+		check that specific component specification has correct lifetime non-declaration
+		"""
 		self.assertTrue(self._configuration_["RemoveWordDefinitionTask"].lifetime() == "")
 
 if __name__ == '__main__':
--- Tests/ManagerTests.py
+++ Tests/ManagerTests.py
@@ -3,12 +3,63 @@ __author__ = 'ND'
 import unittest
 import Declare
 
-class MyTestCase(unittest.TestCase):
 
-	_configuration_ = None
+from TestModel import UserTask
+
+class ManagerTests(unittest.TestCase):
+	"""
+	Declare.Manager test scenarios
+	"""
+
+	_manager_ = None
 
 	def setUp(self):
-		self._configuration_ = Declare.Configuration.read("configuration.json")
+		"""
+		read configuration and create manager
+		"""
+		configuration = Declare.Configuration.read("configuration.json")
+		self._manager_ = Declare.Manager(configuration)
+
+	def test_singleton_by_type(self):
+		"""
+		check that singleton of specific type or base type exists
+		"""
+		tasks = self._manager_.get_components_of_type(UserTask, "singleton")
+
+		self.assertTrue(len(tasks) == 1 and tasks[0].__task_name__ == "ListWordDefinitionsTask")
+
+	def test_non_singleton_by_type(self):
+		"""
+		check that retrieving non singleton by type returns correct result
+		"""
+		tasks = self._manager_.get_components_of_type(UserTask, "")
+
+		tasksFound = []
+
+		for task in tasks:
+			tasksFound.append(task.__task_name__)
+
+		self.assertTrue(len(tasksFound) == 2 and "AddWordDefinitionTask" in tasksFound and "RemoveWordDefinitionTask" in tasksFound)
+
+	def test_component_by_identifier(self):
+		"""
+		check that all components are obtainable by identifier
+		"""
+		addWordTask = self._manager_.get_component("AddWordDefinitionTask")
+		listWordsTask = self._manager_.get_component("ListWordDefinitionsTask")
+		removeWordTask = self._manager_.get_component("RemoveWordDefinitionTask")
+
+		self.assertTrue(not (addWordTask is None or listWordsTask is None or removeWordTask is None))
+
+	def test_component_init_argument_correctness(self):
+		"""
+		check that components declared with init args has correct init args
+		"""
+		addWordTask = self._manager_.get_component("AddWordDefinitionTask")
+		removeWordTask = self._manager_.get_component("RemoveWordDefinitionTask")
+
+		self.assertTrue(addWordTask.Repeats() == True and removeWordTask.Repeats() == False)
+
 
 if __name__ == '__main__':
 	unittest.main()
